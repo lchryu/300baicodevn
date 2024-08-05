81d0e8f17bf33a23eb1a83cfac4c2dcaa133db9b
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2024-08-06 02:50:34 +0700

    update

--- 300baicode/96.cpp
+++ 300baicode/96.cpp
@@ -1,21 +1,8 @@
 #include <bits/stdc++.h>
-#define FOR(i, a, b) for (int i = a; i <= b; i++)
 #define all(x) x.begin(), x.end()
 #define pb push_back
 using namespace std;
-using ll = long long;
-
-void fastIO() {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr); 
-}
-
 int main() {
-    fastIO();
     int n; cin >> n;
     int a[n];
     vector<int>evenNumbers;
@@ -23,6 +10,6 @@ int main() {
         cin >> a[i];
         if (a[i] % 2 == 0) evenNumbers.pb(a[i]);
     }
-    if (evenNumbers.empty()) cout << 0;
+    if (evenNumbers.empty()) cout << "0.00";
     else cout << fixed << setprecision(2) << accumulate(all(evenNumbers), 0.0)/evenNumbers.size();
 }
