ff681af7dd26027ad0274fb710098101b22f9723
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2024-08-10 22:44:13 +0700

    fix 6.cpp

--- DeQuy/6.cpp
+++ DeQuy/6.cpp
@@ -1,10 +1,11 @@
 #include <bits/stdc++.h>
+using namespace std;
 int fibo(int n) {
     if (n == 1) return 0;
-    if (n == 0) return 1;
+    if (n == 2) return 1;
     return fibo(n - 1) + fibo(n - 2);
 }
 int main() {
-    int n; std::cin >> n;
-    std::cout << fibo(n);
+    int n; cin >> n;
+    cout << fibo(n);
 }
