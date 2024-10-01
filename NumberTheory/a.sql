7282cd3ecafab4eca9673e3aa3475a68c42af3a7
Lương Chung Hội <87046628+lchryu@users.noreply.github.com>
2024-10-01 16:07:47 +0700

    update

--- NumberTheory/a.sql
+++
@@ -1,7 +0,0 @@
-SELECT poi.pointOfInterestID, poi.name, poi.typeOfPOI
-FROM PointOfInterest poi
-LEFT JOIN ViewingInstructions vi ON poi.pointOfInterestID = vi.pointOfInterestID
-LEFT JOIN Location loc ON vi.locationID = loc.locationID
-LEFT JOIN Waypoint wp ON loc.locationID = wp.locationID
-LEFT JOIN PlannedLocationSelection pls ON wp.routeID = pls.routeID
-WHERE pls.planID IS NULL;
